5c2466a6eb00f337b0d72c0b8f1468308449f2e5
Thomas van Erven <thomasvanerven@DANSs-MBP-2.ia.knaw.local>
2022-10-17 15:51:40 +0200

    Logging to cloudwatch task added.

--- email_sanitizer.py
+++ email_sanitizer.py
@@ -1,6 +1,7 @@
 import requests
-
+import boto3
 from prefect import Task
+import datetime
 
 
 @Task
@@ -20,3 +21,24 @@ def submit_to_email_sanitizer(metadata: str, replacement_email: str = ""):
         return response
     else:
         raise Exception(f"Unexpected response code: {response.status_code}. Details: {response.__dict__}")
+
+
+@Task
+def log_to_cloudwatch(
+        message: str,
+        aws_region: str = 'eu-west-1'
+):
+    """
+    Simple client to log specific log lines to AWS Cloudwatch.
+    """
+    client = boto3.client('logs', region_name=aws_region)
+    log_event = client.put_log_events(
+        logGroupName=LOG_GROUP_NAME,
+        logStreamName=LOG_STREAM_NAME,
+        logEvents=[
+            {
+                "timestamp": datetime.datetime.now(),
+                "message": message
+            }
+        ]
+    )
